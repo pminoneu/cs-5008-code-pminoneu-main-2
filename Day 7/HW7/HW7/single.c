d5d5e76888563274138fb625da7ec0e66c5e725a
Paula Minozzo <furquimaraujominoz.p@northeastern.edu>
2025-03-02 21:32:29 -0800

    updates

--- Day 7/HW7/HW7/single.c
+++ Day 7/HW7/HW7/single.c
@@ -2,6 +2,24 @@
 #include <stdlib.h> // provides malloc() and free()
 #include <string.h>
 
+// creates a function insertEnd
+void insertEnd(sNode **head, cityData *city) {
+    sNode *newNode = (sNode*)malloc(sizeof(sNode)); // allocates memory for a new node
+    newNode->data = city; // assigns the data to the new node
+    newNode->next = NULL; // assigns the next pointer to NULL
+
+    if (*head == NULL) { // if the list is empty
+        *head = newNode; // the new node is the head
+    } else {
+        sNode *current = *head; // creates a pointer to the head
+        while (current->next != NULL) { // loops until the end of the list
+            current = current->next; // moves to the next node
+        }
+        current->next = newNode; // assigns the new node to the end of the list
+    }
+
+}
+
 // creates a struct to hold city data
 typedef struct 
 {
